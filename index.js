03a85cb70e85b7f7310a91fbaffaf199b5e271ca
sudhanshukuumar <116362103+sudhanshukuumar@users.noreply.github.com>
2024-09-07 16:53:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin: ['http://localhost:5173','https://talaentify.vercel.app','https://talentify-nine.vercel.app'],
+    origin: ['http://localhost:5173','https://talentify-nine.vercel.app'],
     credentials: true
 };
 app.use(cors(corsOptions));
