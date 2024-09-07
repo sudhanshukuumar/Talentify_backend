56d59803b9e0d05af7399ede3993bf40654d377b
sudhanshukuumar <116362103+sudhanshukuumar@users.noreply.github.com>
2024-09-07 16:40:38 +0530

    Update isAuthenticated.js

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -9,7 +9,9 @@ const isAuthenticated = async (req, res, next) => {
                 success: false,
             })
         }
-        const decode = await jwt.verify(token, process.env.SECRET_KEY);
+
+        const secretKey = "xcbfbvnd";
+        const decode = await jwt.verify(token, secretKey );
         if(!decode){
             return res.status(401).json({
                 message:"Invalid token",
